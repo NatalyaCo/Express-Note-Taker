caf98e674d81e81daf9acdd07ab8f6fdb8063297
Natalya Colburn <greatqueenbee@gmail.com>
2022-05-28 10:41:02 -0600

    did the functions for helper to read and write note

--- helper/fsHelper.js
+++ helper/fsHelper.js
@@ -5,5 +5,41 @@ const readFromFile = util.promisify(fs.readFile);
 
 
 
+const writeToFile = (destination, content) => {
 
-module.exports = {readFromFile, writeToFile, readAppend};
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => {
+
+        if (err) {
+            console.error(err);
+
+        } else {
+            console.info(`\nData written to ${destination}`);
+        }
+    });
+};
+
+
+
+const readAppend = (content, file) => {
+
+    fs.readFile(file, 'utf8', (err, data) => {
+
+        if (err) {
+            console.error(err);
+
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+
+
+module.exports = {
+    readFromFile,
+    writeToFile,
+    readAppend
+};
+
