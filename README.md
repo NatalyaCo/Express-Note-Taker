9c6537e00f053020eb4e71bac55019f01e164014
Natalya Colburn <greatqueenbee@gmail.com>
2022-07-05 13:47:43 -0600

    updated README

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Express-Note-Taker
-Backend app that you can use to make notes for yourself
+# Express Note Taker  
+[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+This is a backend application that allows user to take notes and save them in database. You can add, view, eidt, and delete notes.
+## Table of Contents:
+* [Installation](#install)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contribute)
+* [Tests](#tests)
+* [Questions](#questions)
+### Installation:
+In order to install the necessary dependencies, open the console and run the following:
+```Node.js and other dependencies must be installed.```
+### Usage:
+This is a backend only application, no front end is available for users. 
+### License:
+This project is licensed under:
+None
+### Contributing:
+feel free to clone it and make it better how you see fit.
+### Tests:
+In order to test open the console and run the following:
+```no tests available for this applicaiton.```
+### Questions:
+If you have any questions contact me on [GitHub](https://github.com/natalyaco) or contact 
+Natalya Colburn at natalya.c2022@yahoo.com  
+ 
--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,69 @@
 const notes = require('express').Router();
-const {readFromFile, readAppend, writeToFile} = require('../helper/fsHelper');
+const {
+    readFromFile,
+    readAppend,
+    writeToFile
+} = require('../helper/fsHelper');
+
+const ShortUniqueId = require('short-unique-id');
 
 
 notes.get('/', (req, res) => {
-console.log(`${req.method} received`);
-   
-readFromFile('./db/db.json').then((data) => {res.json(JSON.parse(data));
+    console.log(`${req.method} received`);
+
+    readFromFile('./db/db.json').then((data) => {
+        res.json(JSON.parse(data));
     });
 });
+
+notes.post('/', (req, res) => {
+    console.log(`${req.method} received`);
+
+    const {
+        title,
+        text
+    } = req.body;
+    if (title && text) {
+        const uid = new ShortUniqueId({
+            length: 4
+        });
+
+        const newNote = {
+            title,
+            text,
+            id: uid()
+        };
+        console.log(newNote);
+        readAppend(newNote, './db/db.json');
+
+        const response = {
+            status: "success",
+            body: newNote
+        };
+
+        res.json(response);
+    } else {
+        res.json("Error in posting note");
+    }
+});
+
+notes.delete('/:id', (req, res) => {
+
+    if (req.params.id) {
+        console.log(`${req.method} received`);
+        const noteId = req.params.id;
+        const filteredDbArr = [];
+        readFromFile('./db/db.json').then((data) => {
+            dbArr = JSON.parse(data);
+
+            filteredDbArr.push(...dbArr.filter((obj) => obj.id != noteId));
+            writeToFile('./db/db.json', filteredDbArr);
+
+            res.json(filteredDbArr);
+        })
+    } else {
+        res.json("Error in deleting note");
+    }
+});
+
+module.exports = notes;
